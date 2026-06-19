fddfd944ee49584c2d8a2fead54bc6ec7299f900
sNeo7 <salman.almadouh7@gmail.com>
2026-06-19 16:19:18 +0300

    Create firebase.js

---
+++ firebase.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBOK2kQv1IThyAbQ_Ym8y1pDHsXmvdkNXs",
+  authDomain: "brainboost-5c838.firebaseapp.com",
+  projectId: "brainboost-5c838",
+  storageBucket: "brainboost-5c838.firebasestorage.app",
+  messagingSenderId: "643262204761",
+  appId: "1:643262204761:web:7e4e9d6d1c6d971ffc5a48",
+  measurementId: "G-ZWRBB1Q827"
+};
+
+firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
